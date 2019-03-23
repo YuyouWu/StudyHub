946ee3faf804706ae9ce834d7f35f2013a6572d4
YuyouWu <yuyouwu5@gmail.com>
2019-03-22 19:14:47 -0700

    Temporary solution for rendering class menu

--- client/src/components/ClassMenu.js
+++ client/src/components/ClassMenu.js
@@ -5,43 +5,64 @@ import { getClasses, getClass } from '../actions/classActions';
 import { Menu } from 'antd';
 
 class ClassMenu extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            classes: [],
-            loading: true
+            classes: [''],
+            loading: true,
+            forceUpdate: ''
         }
     }
 
-    async componentDidMount(){
-        let classes = [];
-        await this.props.getClasses().then((res)=> {
+    async componentDidMount() {
+        var classes = [];
+        await this.props.getClasses().then((res) => {
             res.payload.forEach(subject => {
                 this.props.getClass(subject).then(classObj => {
                     classes.push(classObj.payload.data);
+                    this.setState({
+                        forceUpdate: classObj.payload.data
+                    });
                 });
             });
         });
         this.setState({
             classes: classes,
             loading: false
+        }, () => {
+            console.log(this.state);
         });
+
+        // setTimeout(() => {
+        //     this.setState({
+        //         classes: classes,
+        //         loading: false
+        //     }, () => {
+        //         console.log(this.state);
+        //     });
+        // }, 1000);
     }
 
-    render(){
+    render() {
         return (
             <div>
                 <h1 style={{ color: 'white', marginLeft: '30px', marginTop: '20px', marginBottom: '15px' }}>Planista</h1>
                 <Menu
                     theme='dark'
-                    style={{ width: '100%'}}
+                    style={{ width: '100%' }}
                     mode="inline"
                 >
-                    {this.state.classes.map((classObj)=> {
-                        return (
-                            <Menu.Item key={classObj._id}>{classObj.name + ' ' + classObj.number}</Menu.Item>
-                        );
-                    })}
+                    {!this.state.loading ?
+                        (
+                            this.state.classes.map((obj) => {
+                                return (
+                                    <Menu.Item key={obj._id}>{obj.name + ' ' + obj.number}</Menu.Item>
+                                );
+                            })
+                        ) : (
+                            <Menu.Item>Loading</Menu.Item>
+                        )
+                    }
                 </Menu>
             </div>
         );
